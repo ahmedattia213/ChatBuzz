bd1f105c028f98246ff344a4661a0f61bb7e18e2
Ahmed Amr <amr@Ahmeds-MacBook-Air.local>
2018-05-16 07:07:37 +0200

    Initial commit

---
+++ ChatBuzz/WelcomeViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChatBuzz
+//
+//  Created by Ahmed Amr on 5/16/18.
+//  Copyright © 2018 Ahmed Amr. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
