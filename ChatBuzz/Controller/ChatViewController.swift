0213589615a0f3894102af0649f2899fa96737fc
Ahmed Amr <amr@Ahmeds-MacBook-Air.local>
2018-05-23 05:24:37 +0200

    Table view completed and can display data

--- ChatBuzz/Controller/ChatViewController.swift
+++ ChatBuzz/Controller/ChatViewController.swift
@@ -8,13 +8,19 @@
 
 import UIKit
 
-class ChatViewController: UIViewController {
+class ChatViewController: UIViewController, UITableViewDelegate , UITableViewDataSource {
 
     @IBOutlet weak var messageTextField: UITextField!
+    @IBOutlet weak var messageTableView: UITableView!
+    @IBOutlet weak var sendButton: UIButton!
+    let messageArray = ["First message", "Scond" , " blabla"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+     messageTableView.delegate = self
+     messageTableView.dataSource = self
+     messageTableView.register(UINib(nibName: "MessageCell", bundle: nil), forCellReuseIdentifier: "customMessageCell")
+        configureTableView()
         // Do any additional setup after loading the view.
     }
 
@@ -28,6 +34,23 @@ class ChatViewController: UIViewController {
     
  
     @IBAction func sendButtonPressed(_ sender: UIButton) {
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "customMessageCell", for: indexPath) as! CustomMessageCell
+        
+        cell.messageBody.text = messageArray[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messageArray.count
+    }
+    
+    func configureTableView() {
+        messageTableView.rowHeight = UITableViewAutomaticDimension
+        messageTableView.estimatedRowHeight = 120.0
     }
     
     /*
